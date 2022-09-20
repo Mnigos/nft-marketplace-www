6e682ff8d216e98cdc32c9f309b8c94b19957ece
Mnigos <makowskiigor@gmail.com>
2022-09-20 12:35:40 +0200

    chore(tests): transpile `vuetify`

--- setup-tests.ts
+++ setup-tests.ts
@@ -2,3 +2,5 @@ import { setActivePinia, createPinia } from 'pinia'
 import '@testing-library/jest-dom/extend-expect'
 
 setActivePinia(createPinia())
+
+global.CSS = { supports: () => false, escape: () => '' }

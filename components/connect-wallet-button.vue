30ed6ab0c1041a779ea77fd34d63a9a71f948909
Mnigos <makowskiigor@gmail.com>
2022-09-23 16:24:29 +0200

    feat(connect-wallet-button): connect with metamask on click

--- components/connect-wallet-button.vue
+++ components/connect-wallet-button.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { useWalletStore } from '~/stores'
+import { addressFormatter } from '~/utils'
+
+const walletStore = useWalletStore()
+</script>
+
 <template>
-  <v-btn rounded="lg" color="primary" variant="elevated">
-    connect wallet
+  <v-btn
+    rounded="lg"
+    color="primary"
+    variant="elevated"
+    @click="walletStore.connect"
+  >
+    <span v-if="walletStore.isConnected">
+      {{ addressFormatter(walletStore.account) }}
+    </span>
+
+    <span v-else>Connect Wallet</span>
   </v-btn>
 </template>

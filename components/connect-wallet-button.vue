a642c35c4d67b7df735086bcd365477805402eef
Mnigos <makowskiigor@gmail.com>
2022-09-27 17:12:28 +0200

    feat(connect-wallet-button): add alert when no metamask installed

--- components/connect-wallet-button.vue
+++ components/connect-wallet-button.vue
@@ -3,19 +3,40 @@ import { useWalletStore } from '~/stores'
 import { addressFormatter } from '~/utils'
 
 const walletStore = useWalletStore()
+
+const isDialogOpen = ref(false)
+
+function connectWallet() {
+  if (!window.ethereum) return (isDialogOpen.value = true)
+
+  walletStore.connect()
+}
 </script>
 
 <template>
-  <v-btn
-    rounded="lg"
-    color="primary"
-    variant="elevated"
-    @click="walletStore.connect"
-  >
+  <v-btn rounded="lg" color="primary" variant="elevated" @click="connectWallet">
     <span v-if="walletStore.isConnected">
       {{ addressFormatter(walletStore.account) }}
     </span>
 
     <span v-else>Connect Wallet</span>
+
+    <v-dialog v-model="isDialogOpen">
+      <v-alert type="error" variant="flat">
+        <div w:flex="~ gap-4" w:items="center">
+          <p>Oops. Looks like you don't have installed metamask extension</p>
+
+          <v-btn color="white" variant="outlined" @click="isDialogOpen = false">
+            Close
+          </v-btn>
+        </div>
+      </v-alert>
+    </v-dialog>
   </v-btn>
 </template>
+
+<style>
+.v-overlay__content {
+  width: auto !important;
+}
+</style>

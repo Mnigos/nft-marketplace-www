6e69bebcf55fdeb7053bedc105a81284072d77da
Mnigos <makowskiigor@gmail.com>
2022-09-27 15:12:27 +0200

    feat(composables): create `use-is-collapsed`

---
+++ composables/use-is-collapsed.ts
@@ -0,0 +1,37 @@
+export enum ScrollDirection {
+  NONE,
+  UP,
+  DOWN,
+}
+
+export function useIsCollapsed() {
+  const scrollDirection = ref(ScrollDirection.NONE)
+  const isCollapsed = ref(false)
+  const lastScrollPosition = ref(0)
+
+  const toggleCollapsed = () => {
+    isCollapsed.value = !isCollapsed.value
+  }
+
+  function handleScroll() {
+    const currentScrollPosition = window.pageYOffset
+
+    scrollDirection.value =
+      currentScrollPosition > lastScrollPosition.value
+        ? ScrollDirection.DOWN
+        : ScrollDirection.UP
+
+    isCollapsed.value =
+      currentScrollPosition > 0
+        ? scrollDirection.value === ScrollDirection.UP
+        : true
+
+    isCollapsed.value = currentScrollPosition > lastScrollPosition.value
+    lastScrollPosition.value = currentScrollPosition
+  }
+
+  onMounted(() => window.addEventListener('scroll', handleScroll))
+  onBeforeUnmount(() => window.addEventListener('scroll', handleScroll))
+
+  return { isCollapsed, toggleCollapsed, scrollDirection }
+}

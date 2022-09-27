44f12da1599fdead782fcc9fa404dd0d69961a36
Mnigos <makowskiigor@gmail.com>
2022-09-27 17:55:36 +0200

    fix(navigation-bar): collapsing

--- stores/navigation.ts
+++ stores/navigation.ts
@@ -3,10 +3,14 @@ import { defineStore } from 'pinia'
 export const useNavigationStore = defineStore('navigation', {
   state: () => ({
     isDrawerOpen: false,
+    isDialogOpen: false,
   }),
   actions: {
     toggleDrawer() {
       this.isDrawerOpen = !this.isDrawerOpen
+    },
+    toggleDialog() {
+      this.isDialogOpen = !this.isDialogOpen
     },
   },
 })

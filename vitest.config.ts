254a23a6710f53510c59ffc1619a8a814337c965
Mnigos <makowskiigor@gmail.com>
2022-09-27 15:34:12 +0200

    chore(dev-deps): add `unplugin-auto-import`

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,16 @@ import { fileURLToPath } from 'url'
 
 import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [Vue()],
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: '.nuxt/imports.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./', import.meta.url)),
